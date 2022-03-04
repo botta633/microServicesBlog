2016265dadc093ee8b3ce16ea2b055eb2292e742
Ahmed <ahmed@Ahmeds-MacBook-Pro-2.local>
2022-03-04 15:53:06 +0200

    adding storage to event-bus to synchronize all newly added services

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,9 +6,10 @@ const axios = require('axios')
 const app = express();
 app.use(bodyParser.json());
 
-
+const events = [];
 app.post('/events', (req, res)=>{
     const event = req.body;
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event).catch((err) => {
         console.log(err.message);
@@ -25,6 +26,10 @@ app.post('/events', (req, res)=>{
       res.send(
         {status: 'OK'}
     );
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 
--- query/index.js
+++ query/index.js
@@ -11,15 +11,8 @@ app.use(cors());
 
 const posts = {}
 
+const handleEvent = (type, data) => {
 
-app.get("/posts", (req, res) =>{
-
-    res.send(posts);
-
-});
-app.post("/events", (req, res) =>{
-    const {type, data } = req.body
-    
     if(type === 'PostCreated'){
         const {id, title} = data;
         posts[id] = {id, title, comments: []};
@@ -41,14 +34,28 @@ app.post("/events", (req, res) =>{
         comment.status = status;
         comment.content = content;
     }
+}
 
-    console.log(posts);
+app.get("/posts", (req, res) =>{
+
+    res.send(posts);
+
+});
+app.post("/events", (req, res) =>{
+    const {type, data } = req.body
+
+    handleEvent(type, data);
     res.send({});
 });
 
 
 
 
-app.listen(4002, ()=>{
+app.listen(4002, async ()=>{
     console.log("listening on port 4002");
+    const res = await axios.get('http://localhost:4005/events');
+    for (let event of res.data){
+        console.log('Processing event:', event.type);
+        handleEvent(event.type, event.data)
+    }
 })
